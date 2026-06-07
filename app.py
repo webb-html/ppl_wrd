f9e703b5a7180ab7261566b873181b49a76a71e9
Arseniy.shrk <arseniy.shrk@gmail.com>
2026-06-07 15:58:04 +0700

    testing github deploy

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+import os
+from waitress import serve
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +11,7 @@ def index():
     return '''
     hello world! -tessto
     '''
+
+if __name__ == '__main__':
+    print('starting app {{ 127.0.0.1:5000 }}')
+    serve(app, port=os.environ.get("PORT", 5000), host='0.0.0.0')
